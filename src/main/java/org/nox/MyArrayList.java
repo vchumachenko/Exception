bbfda23daf3deee63c559c41623892116b5c440f
vchumachenko <v.chumachenko@yahoo.com>
2023-09-27 15:59:09 +0300

    master Update

--- src/main/java/org/nox/MyArrayList.java
+++ src/main/java/org/nox/MyArrayList.java
@@ -5,17 +5,10 @@ import java.util.ArrayList;
 public class MyArrayList<T> {
     private ArrayList<T> list = new ArrayList<>();
 
-    public void addElement(T element) throws ArrayListFullException{
-        if(list.size() >= 10){
+    public void addElement(T element) throws ArrayListFullException {
+        if (list.size() >= 10) {
             throw new ArrayListFullException();
         }
         list.add(element);
-    }
-
-    @Override
-    public String toString() {
-        return "MyArrayList{" +
-                "list=" + list +
-                '}';
     }
 }
